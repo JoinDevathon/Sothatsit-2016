2c6638d5ec59f0c5f55dc7c2624ef36572c3198e
BuildTools <so.thatsit@hotmail.com>
2016-11-06 08:34:02 +0800

    add MachinePiece saving/loading

--- src/main/java/net/sothatsit/devathon2016/machine/MachinePiece.java
+++ src/main/java/net/sothatsit/devathon2016/machine/MachinePiece.java
@@ -2,15 +2,26 @@ package net.sothatsit.devathon2016.machine;
 
 import net.sothatsit.devathon2016.blocks.Offset;
 import net.sothatsit.devathon2016.blocks.Schematic;
+import org.bukkit.configuration.ConfigurationSection;
 
-public abstract class MachinePiece {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
 
+public class MachinePiece {
+
+    private PieceType type;
     private Offset[] offsets;
     private Schematic schematic;
 
-    public MachinePiece(Offset[] offsets, Schematic schematic) {
+    public MachinePiece(PieceType type, Offset[] offsets, Schematic schematic) {
+        this.type = type;
         this.offsets = offsets;
         this.schematic = schematic;
+    }
+
+    public PieceType getType() {
+        return this.type;
     }
 
     public Offset getOffset(int index) {
@@ -25,8 +36,55 @@ public abstract class MachinePiece {
         return this.schematic;
     }
 
-    public static MachinePiece loadFrom() {
-        return null;
+    public void saveTo(ConfigurationSection section) {
+        List<String> offsetStrings = new ArrayList<>();
+
+        for(Offset offset : this.offsets) {
+            offsetStrings.add(offset.toString());
+        }
+
+        section.set("type", this.type.getName());
+        section.set("offsets", offsetStrings);
+
+        this.schematic.saveTo(section);
+    }
+
+    public static MachinePiece loadFrom(ConfigurationSection section, Logger logger) {
+        if(!section.isSet("type") || !section.isSet("offsets")
+                || !section.isString("type") || !section.isList("offsets")) {
+            logger.info("Type or Offsets unset at \"" + section.getCurrentPath() + "\"");
+            return null;
+        }
+
+        PieceType type = PieceType.fromName(section.getString("type"));
+
+        if(type == null) {
+            logger.info("Unable to load piece type from \"" + section.getCurrentPath() + "\"");
+            return null;
+        }
+
+        List<String> offsetStrings = section.getStringList("offsets");
+        Offset[] offsets = new Offset[offsetStrings.size()];
+
+        for(int index = 0; index < offsets.length; index++) {
+            String offsetString = offsetStrings.get(index);
+            Offset offset = Offset.fromString(offsetString);
+
+            if(offset == null) {
+                logger.info("Unable to load offset from \"" + section.getCurrentPath() + "\"");
+                return null;
+            }
+
+            offsets[index] = offset;
+        }
+
+        Schematic schematic = Schematic.loadFrom(section, logger);
+
+        if(schematic == null) {
+            return null;
+        }
+
+        return new MachinePiece(type, offsets, schematic);
     }
 
 }
